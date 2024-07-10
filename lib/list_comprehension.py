94d3e171c3ecdb5494c324f927673da715f637ea
Guillermo <vital_tmv@yahoo.com>
2024-07-10 11:42:40 -0500

    complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 def return_evens(num_list):
+    number_even = [num for num in num_list if num % 2 == 0]
+    return number_even
     pass
 
-def make_exclamation(sentence_list):
-    pass
+# def make_exclamation(sentence_list):
+#     exclaimed_list = [item + "!" for item in sentence_list]
+#     return exclaimed_list
+#     pass
+   
+
+
